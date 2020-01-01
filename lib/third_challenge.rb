f95288ea595e639dc4ab50131fbffebe4b7f19a5
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-01 19:22:58 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,11 +27,11 @@ def third_challenge
         },
       :heroine => {
         name: "Juliet",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :heroine_friends => []
-   },
+   }
   }
 
 
