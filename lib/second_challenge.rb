d0d9c3ad3021b17f5a84ae8529ca0bab5726f961
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-01 19:14:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,20 +1,20 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-[
+
   patriarch:{},
   matriarch: {},
   hero: {},
   hero_friends: []
-]
+
       },
     :capulet => {
-  [
+  
     patriarch:{},
     matriarch: {},
     heroine: {}, 
     heroine_friends: []
-  ]
+  
       }}
 
 
